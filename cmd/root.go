95d8042302e9d79a84789e19a3da2fe30b4060cc
Vladislav Mitov <vladislav.mitov@gmail.com>
2017-06-20 11:16:28 +0300

    Stop looking for cfg in the current dir if explicitly passed

--- cmd/root.go
+++ cmd/root.go
@@ -53,9 +53,10 @@ func init() {
 func initConfig() {
 	if cfgFile != "" { // enable ability to specify config file via flag
 		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.AddConfigPath(".")
 	}
 
-	viper.AddConfigPath(".")
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// Read config file
