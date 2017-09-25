654ede3afcb626805ad67db67c3882c22d55f08c
Vladislav Mitov <vladislav.mitov@gmail.com>
2017-09-25 22:50:45 +0300

    Add integration test with Consul

---
+++ diff/diff_test.go
@@ -0,0 +1,6 @@
+package diff
+
+import "flag"
+
+// It is defined in each package so you can run `go test ./...`
+var full = flag.Bool("full", false, "Run all tests including integration")
---
+++ source/source_test.go
@@ -0,0 +1,6 @@
+package source
+
+import "flag"
+
+// It is defined in each package so you can run `go test ./...`
+var full = flag.Bool("full", false, "Run all tests including integration")
